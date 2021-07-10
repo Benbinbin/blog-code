74fcab9d2a882fa5773f3319b790490e1a89232e
Benbinbin <benthomsonbin@gmail.com>
2021-07-10 15:34:27 +0800

    add algorithm classification.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -47,15 +47,19 @@ module.exports = {
           name: "Github",
           image: "github.svg"
         },
+        {
+          name: "Algorithm",
+          image: "algorithm.svg"
+        },
       ]
     }],
     [require('./plugins/generateListPages.js'),
     {
-      postClassifications: ['git', 'github']
+      postClassifications: ['git', 'github', 'algorithm']
     }],
     [require('./plugins/generateFolderPages.js'),
     {
-      postFolders: ['git', 'github']
+      postFolders: ['git', 'github', 'algorithm']
     }],
   ],
   theme: path.resolve(__dirname, './theme/index.js'),
@@ -87,8 +91,8 @@ module.exports = {
     __HOME_DESCRIPTION__: "这是我的<a href='https://benbinbin.github.io/tech-blog/' target='_blank' class='hover:text-blue-600 underline font-bold transition-colors'>技术部落格</a>之一，主要内容是与编程相关的笔记。",
     __HOME_PAGE_COLOR__: '#9CA3AF',
     __AVATAR__: 'avatar.png',
-    __CLASSIFICATIONS__: ['All', 'Git', 'Github'],
-    __FOLDERS__: ['Git', 'Github'],
+    __CLASSIFICATIONS__: ['All', 'Git', 'Github', 'Algorithm'],
+    __FOLDERS__: ['Git', 'Github', 'Algorithm'],
     __FOOTER_AVATAR_LINK__: 'https://benbinbin.github.io/',
     __AUTHOR__: 'Benbinbin',
     __FOOTER_LICENSE__: 'CC-BY-SA-4.0',
